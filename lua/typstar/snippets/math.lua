32a1ff65d14d61b79d6a712145c5f39fd8a2c6dc
lentilus <lentilus@mailbox.org>
2025-01-09 01:39:36 +0000

    fix: consitency of sum snippets

--- lua/typstar/snippets/math.lua
+++ lua/typstar/snippets/math.lua
@@ -72,7 +72,8 @@ return {
     snip('oit', 'integral_(Omega}', {}, math),
     snip('dit', 'integral_{<>}', { i(1, 'Omega') }, math),
 
-    snip('sm', 'sum_(<>)^(<>)', { i(1, 'i=0'), i(2, 'oo') }, math),
+    snip('sm', 'sum', {}, math),
+    snip('sum', 'sum_(<>)^(<>)', { i(1, 'i=0'), i(2, 'oo') }, math),
     snip('osm', 'sum_Omega', {}, math),
     snip('dsm', 'sum_(<>)', { i(1, 'I') }, math),
 
