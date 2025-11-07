f4b3d1ec3aacba4e30a88fe2e11746757353baca
Eagle4398 <177400222+Eagle4398@users.noreply.github.com>
2025-11-07 17:29:47 +0100

    fix lua-egex

--- lua/typstar/autosnippets.lua
+++ lua/typstar/autosnippets.lua
@@ -101,7 +101,7 @@ function M.start_snip_in_newl(trigger, expand, insert, condition, priority, opti
         line = M.cap(1)
     else
         -- capture whole line if line needs to be manipulated
-        trigger = '(.*\\S)\\s+' .. trigger
+        trigger = '(.[^\\s])\\s+' .. trigger
         line = M.cap(1, options.transform)
         options = vim.tbl_deep_extend('keep', { indentCaptureIdx = 1 }, options or {})
     end
