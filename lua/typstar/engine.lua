84b1482770043aeedb6ca0dfd1d94bfdf0b40d10
arne314 <mail@arne.one>
2026-02-13 11:10:28 +0100

    fix(snip): detect markup inside math

--- lua/typstar/engine.lua
+++ lua/typstar/engine.lua
@@ -23,6 +23,7 @@ M.in_math = function()
     local cursor = utils.get_cursor_pos()
     return utils.cursor_within_treesitter_query(ts_math_query, 0, 0, cursor)
         and not utils.cursor_within_treesitter_query(ts_string_query, 0, 0, cursor)
+        and M.not_in_markup()
 end
 M.in_markup = function() return utils.cursor_within_treesitter_query(ts_markup_query, 1, 2) end
 M.not_in_math = function() return not M.in_math() end
